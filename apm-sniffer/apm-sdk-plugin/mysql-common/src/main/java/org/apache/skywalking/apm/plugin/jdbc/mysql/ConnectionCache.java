f19195368916e9514d19084eb1a29737c74bd25b
lpcy <70789644+lpcy@users.noreply.github.com>
2021-09-04 22:03:25 +0800

    fix multi-db-instance on same host port (#18)

--- apm-sniffer/apm-sdk-plugin/mysql-common/src/main/java/org/apache/skywalking/apm/plugin/jdbc/mysql/ConnectionCache.java
+++ apm-sniffer/apm-sdk-plugin/mysql-common/src/main/java/org/apache/skywalking/apm/plugin/jdbc/mysql/ConnectionCache.java
@@ -28,19 +28,19 @@ public class ConnectionCache {
 
     private static final String CONNECTION_SPLIT_STR = ",";
 
-    public static ConnectionInfo get(String host, String port) {
-        final String hostPortPair = String.format("%s:%s", host, port);
+    public static ConnectionInfo get(String host, String port, String databaseName) {
+        final String hostPortPair = String.format("%s:%s/%s", host, port, databaseName);
         return CONNECTIONS_MAP.get(hostPortPair);
     }
 
-    public static ConnectionInfo get(String hostPortPair) {
-        return CONNECTIONS_MAP.get(hostPortPair);
+    public static ConnectionInfo get(String hostPortPair, String databaseName) {
+        return CONNECTIONS_MAP.get(hostPortPair + "/" + databaseName);
     }
 
     public static void save(ConnectionInfo connectionInfo) {
         for (String conn : connectionInfo.getDatabasePeer().split(CONNECTION_SPLIT_STR)) {
             if (!StringUtil.isEmpty(conn)) {
-                CONNECTIONS_MAP.putIfAbsent(conn, connectionInfo);
+                CONNECTIONS_MAP.putIfAbsent(conn + "/" + connectionInfo.getDatabaseName(), connectionInfo);
             }
         }
     }

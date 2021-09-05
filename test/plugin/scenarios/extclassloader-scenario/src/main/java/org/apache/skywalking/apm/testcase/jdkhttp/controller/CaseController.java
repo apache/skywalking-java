b72824131f2d222916600e708d3df85a78b6d3ee
daimingzhi <dmzitman@163.com>
2021-09-05 19:26:36 +0800

    test case for extclassloader-scenarion

---
+++ test/plugin/scenarios/extclassloader-scenario/src/main/java/org/apache/skywalking/apm/testcase/jdkhttp/controller/CaseController.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.apm.testcase.jdkhttp.controller;
+
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/case")
+public class CaseController {
+
+
+    private static final String SUCCESS = "Success";
+
+    @RequestMapping("/extclassloader-scenario")
+    @ResponseBody
+    public String testcase(HttpServletRequest request) throws IOException {
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://localhost:8080" + request.getContextPath() + "/case/receiveContext-0");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        try {
+            HttpEntity entity = response.getEntity();
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+            httpclient.close();
+        }
+
+        return SUCCESS;
+    }
+
+    @RequestMapping("/healthCheck")
+    @ResponseBody
+    public String healthCheck() {
+        // your codes
+        return SUCCESS;
+    }
+
+    @RequestMapping("/receiveContext-0")
+    @ResponseBody
+    public String receiveContextService0() {
+        return "receiveContext-0";
+    }
+
+}

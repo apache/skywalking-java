153ff9f1c360e6c65cb9923677af21450b802f83
daimingzhi <dmzitman@163.com>
2021-09-07 11:01:33 +0800

    fix ci issue

--- test/plugin/scenarios/extclassloader-scenario/src/main/java/org/apache/skywalking/apm/testcase/extclassloader/controller/CaseController.java
+++ test/plugin/scenarios/extclassloader-scenario/src/main/java/org/apache/skywalking/apm/testcase/extclassloader/controller/CaseController.java
@@ -19,6 +19,7 @@
 package org.apache.skywalking.apm.testcase.extclassloader.controller;
 
 import java.io.IOException;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -29,44 +30,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/case")
 public class CaseController {
 
-    private static final String SUCCESS = "Success";
+	private static final String SUCCESS = "Success";
 
-    @RequestMapping("/extclassloader-scenario")
-    @ResponseBody
-    public String testcase(HttpServletRequest request) throws IOException {
+	@RequestMapping("/extclassloader-scenario")
+	@ResponseBody
+	public String testcase(HttpServletRequest request) throws IOException {
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("http://localhost:8080" + request.getContextPath() + "/case/receiveContext-0");
-        CloseableHttpResponse response = httpclient.execute(httpGet);
-        try {
-            HttpEntity entity = response.getEntity();
-            EntityUtils.consume(entity);
-        } finally {
-            response.close();
-            httpclient.close();
-        }
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet("http://localhost:8080" + request.getContextPath() + "/case/receiveContext-0");
+		CloseableHttpResponse response = httpclient.execute(httpGet);
+		try {
+			HttpEntity entity = response.getEntity();
+			EntityUtils.consume(entity);
+		} finally {
+			response.close();
+			httpclient.close();
+		}
 
-        return SUCCESS;
-    }
+		return SUCCESS;
+	}
 
-    @RequestMapping("/healthCheck")
-    @ResponseBody
-    public String healthCheck() {
-        // your codes
-        return SUCCESS;
-    }
+	@RequestMapping("/healthCheck")
+	@ResponseBody
+	public String healthCheck() {
+		// your codes
+		return SUCCESS;
+	}
 
-    @RequestMapping("/receiveContext-0")
-    @ResponseBody
-    public String receiveContextService0() {
-        return "receiveContext-0";
-    }
+	@RequestMapping("/receiveContext-0")
+	@ResponseBody
+	public String receiveContextService0() {
+		return "receiveContext-0";
+	}
 
 }

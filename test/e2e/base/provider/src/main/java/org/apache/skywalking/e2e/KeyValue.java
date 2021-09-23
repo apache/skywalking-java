7d36faaf0f46dfd1f7da0df4caa92ab3f52200f5
Daming <zteny@foxmail.com>
2021-09-23 10:50:34 +0800

    Add Kafka LogReporter and make grpc log reporter sharing grpc channel with reporters (#20)

--- test/e2e/base/provider/src/main/java/org/apache/skywalking/e2e/KeyValue.java
+++ test/e2e/base/provider/src/main/java/org/apache/skywalking/e2e/KeyValue.java
@@ -6,31 +6,24 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-package org.apache.skywalking.apm.toolkit.logging.common.log;
+package org.apache.skywalking.e2e;
 
-import org.apache.skywalking.apm.agent.core.boot.PluginConfig;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
-public class ToolkitConfig {
+@Data
+@Accessors(chain = true)
+public class KeyValue {
 
-    public static class Plugin {
-        public static class Toolkit {
-            @PluginConfig(root = ToolkitConfig.class)
-            public static class Log {
-                /**
-                 * Whether or not to transmit logged data as formatted or un-formatted.
-                 */
-                public static boolean TRANSMIT_FORMATTED = true;
-            }
-        }
-    }
+    private String key;
+    private String value;
 }
--- test/e2e/base/provider/src/main/java/org/apache/skywalking/e2e/UserRepo.java
+++ test/e2e/base/provider/src/main/java/org/apache/skywalking/e2e/UserRepo.java
@@ -16,21 +16,9 @@
  *
  */
 
-package org.apache.skywalking.apm.toolkit.logging.common.log;
+package org.apache.skywalking.e2e;
 
-import org.apache.skywalking.apm.agent.core.boot.PluginConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class ToolkitConfig {
-
-    public static class Plugin {
-        public static class Toolkit {
-            @PluginConfig(root = ToolkitConfig.class)
-            public static class Log {
-                /**
-                 * Whether or not to transmit logged data as formatted or un-formatted.
-                 */
-                public static boolean TRANSMIT_FORMATTED = true;
-            }
-        }
-    }
+public interface UserRepo extends JpaRepository<User, Long> {
 }

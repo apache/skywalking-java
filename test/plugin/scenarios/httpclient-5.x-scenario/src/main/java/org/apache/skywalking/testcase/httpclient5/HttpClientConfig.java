3494b7bf87bf3ddbc2256e64cc6e8ad01ce4c5e0
CharliePu <heihaozi2006@163.com>
2021-09-23 15:33:40 +0800

    httpclient-5.x-scenario

---
+++ test/plugin/scenarios/httpclient-5.x-scenario/src/main/java/org/apache/skywalking/testcase/httpclient5/HttpClientConfig.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.testcase.httpclient5;
+
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class HttpClientConfig {
+
+    private static final Logger LOGGER = LogManager.getLogger(HttpClientConfig.class);
+
+    @Bean
+    public CloseableHttpClient getCloseableHttpClient() {
+        return HttpClients.createDefault();
+    }
+
+    @Bean
+    public CloseableHttpAsyncClient getCloseableHttpAsyncClient() {
+        CloseableHttpAsyncClient httpAsyncClient = HttpAsyncClients.createDefault();
+        httpAsyncClient.start();
+        return httpAsyncClient;
+    }
+
+    @Autowired
+    private CloseableHttpClient httpClient;
+
+    @Autowired
+    private CloseableHttpAsyncClient httpAsyncClient;
+
+    @PreDestroy
+    public void destroy() {
+        try {
+            httpClient.close();
+        } catch (Exception e) {
+            LOGGER.error("httpClient close failed", e);
+        }
+        try {
+            httpAsyncClient.close();
+        } catch (Exception e) {
+            LOGGER.error("httpAsyncClient close failed", e);
+        }
+    }
+}

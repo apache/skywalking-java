d5ea6b6b4d9226c7c8add6fba59a8944a6c35569
daming <zteny@foxmail.com>
2021-10-31 01:29:11 +0800

    fix jdk17 test

--- test/plugin/scenarios/jdk17-with-gson-scenario/src/main/java/test/apache/skywalking/apm/testcase/gson/controller/CaseController.java
+++ test/plugin/scenarios/jdk17-with-gson-scenario/src/main/java/test/apache/skywalking/apm/testcase/gson/controller/CaseController.java
@@ -19,6 +19,17 @@
 package test.apache.skywalking.apm.testcase.gson.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import org.apache.skywalking.apm.toolkit.trace.ActiveSpan;
 import org.apache.skywalking.apm.toolkit.trace.Trace;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,8 +45,10 @@ public class CaseController {
     @RequestMapping("/gson-scenario")
     @ResponseBody
     public String gsonCase() {
-        Gson gson = new Gson();
-        Person person = new Person("skywalking", "jdk14");
+        var gson = new GsonBuilder()
+            .registerTypeAdapterFactory(new RecordTypeAdapterFactory())
+            .create();
+        Person person = new Person("skywalking", "jdk17");
         gson.fromJson(gson.toJson(person), Person.class);
         person.action();
         return SUCCESS;
@@ -51,6 +64,62 @@ public class CaseController {
         @Trace(operationName = "/person/action")
         public void action() {
             ActiveSpan.tag("key", "value");
+        }
+    }
+
+    public static class RecordTypeAdapterFactory implements TypeAdapterFactory {
+
+        @Override
+        public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+            @SuppressWarnings("unchecked")
+            Class<T> clazz = (Class<T>) type.getRawType();
+            if (!clazz.isRecord()) {
+                return null;
+            }
+            TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
+
+            return new TypeAdapter<T>() {
+                @Override
+                public void write(JsonWriter out, T value) throws IOException {
+                    delegate.write(out, value);
+                }
+
+                @Override
+                public T read(JsonReader reader) throws IOException {
+                    if (reader.peek() == JsonToken.NULL) {
+                        reader.nextNull();
+                        return null;
+                    } else {
+                        var recordComponents = clazz.getRecordComponents();
+                        var typeMap = new HashMap<String,TypeToken<?>>();
+                        for (int i = 0; i < recordComponents.length; i++) {
+                            typeMap.put(recordComponents[i].getName(), TypeToken.get(recordComponents[i].getGenericType()));
+                        }
+                        var argsMap = new HashMap<String,Object>();
+                        reader.beginObject();
+                        while (reader.hasNext()) {
+                            String name = reader.nextName();
+                            argsMap.put(name, gson.getAdapter(typeMap.get(name)).read(reader));
+                        }
+                        reader.endObject();
+
+                        var argTypes = new Class<?>[recordComponents.length];
+                        var args = new Object[recordComponents.length];
+                        for (int i = 0; i < recordComponents.length; i++) {
+                            argTypes[i] = recordComponents[i].getType();
+                            args[i] = argsMap.get(recordComponents[i].getName());
+                        }
+                        Constructor<T> constructor;
+                        try {
+                            constructor = clazz.getDeclaredConstructor(argTypes);
+                            constructor.setAccessible(true);
+                            return constructor.newInstance(args);
+                        } catch (NoSuchMethodException | InstantiationException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            };
         }
     }
 }

b680745b59f0a4853f3cf87f57928949f1170cdf
guoxiwen <guoxiwen@aspirecn.com>
2022-03-14 19:03:32 +0800

    Added support for webflux using subscribeOn methods

--- test/plugin/scenarios/webflux-scenario/webflux-projectB-scenario/src/main/java/test/apache/skywalking/apm/testcase/sc/webflux/projectB/controller/DbOperate.java
+++ test/plugin/scenarios/webflux-scenario/webflux-projectB-scenario/src/main/java/test/apache/skywalking/apm/testcase/sc/webflux/projectB/controller/DbOperate.java
@@ -4,12 +4,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-/**
- * @ClassName
- * @Description TODO
- * @Author guoxiwen
- * @Date 2022/3/14 18:45
- */
+
 @Service
 public class DbOperate {
     // This method is to generate an annotation trace for testing

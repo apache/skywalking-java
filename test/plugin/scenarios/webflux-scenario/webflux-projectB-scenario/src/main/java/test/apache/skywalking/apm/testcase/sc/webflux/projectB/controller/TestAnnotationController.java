f8d0d7bd966f7b984ea13a7c755cff467ec95a6c
guoxiwen <guoxiwen@aspirecn.com>
2022-03-18 11:57:13 +0800

    code review

--- test/plugin/scenarios/webflux-scenario/webflux-projectB-scenario/src/main/java/test/apache/skywalking/apm/testcase/sc/webflux/projectB/controller/TestAnnotationController.java
+++ test/plugin/scenarios/webflux-scenario/webflux-projectB-scenario/src/main/java/test/apache/skywalking/apm/testcase/sc/webflux/projectB/controller/TestAnnotationController.java
@@ -18,7 +18,12 @@
 package test.apache.skywalking.apm.testcase.sc.webflux.projectB.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import test.apache.skywalking.apm.testcase.sc.webflux.projectB.controller.db.DbOperate;
@@ -63,9 +68,9 @@ public class TestAnnotationController {
     }
 
     @GetMapping("/testcase/subscribeOn/elastic")
-    public Mono<String> boudElastic(@RequestParam(required = true) String body){
+    public Mono<String> boudElastic(@RequestParam(required = true) String body) {
         return Mono.just(body).subscribeOn(Schedulers.elastic()).map(param -> {
-            return dbOperate.selectOne()+param;
+            return dbOperate.selectOne() + param;
         });
 
     }

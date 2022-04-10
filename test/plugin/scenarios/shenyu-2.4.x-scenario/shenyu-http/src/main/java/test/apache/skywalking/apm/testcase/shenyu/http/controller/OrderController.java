27d1173b07bb3488089fd802fc0ff22eee8cad6e
hutaishi <hutaishi@qq.com>
2022-04-10 17:49:10 +0800

    check test module style

--- test/plugin/scenarios/shenyu-2.4.x-scenario/shenyu-http/src/main/java/test/apache/skywalking/apm/testcase/shenyu/http/controller/OrderController.java
+++ test/plugin/scenarios/shenyu-2.4.x-scenario/shenyu-http/src/main/java/test/apache/skywalking/apm/testcase/shenyu/http/controller/OrderController.java
@@ -18,9 +18,6 @@
 package test.apache.skywalking.apm.testcase.shenyu.http.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;

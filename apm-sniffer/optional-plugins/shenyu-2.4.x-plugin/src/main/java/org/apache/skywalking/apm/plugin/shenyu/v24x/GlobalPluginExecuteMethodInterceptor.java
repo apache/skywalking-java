713fd04c80383bab65b77c333664f9609b9e961e
lishuo <lishuo@huobitech.com>
2022-09-14 10:01:15 +0800

    Fix Apache ShenYu plugin traceId empty string value.

--- apm-sniffer/optional-plugins/shenyu-2.4.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/shenyu/v24x/GlobalPluginExecuteMethodInterceptor.java
+++ apm-sniffer/optional-plugins/shenyu-2.4.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/shenyu/v24x/GlobalPluginExecuteMethodInterceptor.java
@@ -77,7 +77,7 @@ public class GlobalPluginExecuteMethodInterceptor implements InstanceMethodsArou
 
         ContextSnapshot snapshot = ContextManager.capture();
         exchange.getAttributes().put(SHENYU_AGENT_TRACE_ID,
-                Optional.ofNullable(snapshot.getTraceId()).map(DistributedTraceId::getId).orElse(""));
+                Optional.ofNullable(snapshot.getTraceId()).map(DistributedTraceId::getId).orElse(ContextManager.getGlobalTraceId()));
         EnhancedInstance instance = getInstance(allArguments[0]);
         instance.setSkyWalkingDynamicField(snapshot);
         span.prepareForAsync();

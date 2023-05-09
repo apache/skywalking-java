4b00fe3b4017929b8fb3b20302fca4bb28c0f71a
guoxiolong <275520895@qq.com>
2023-05-09 23:34:03 +0800

    Add JBoss support through mount agent by ModuleClassloader. (#527)
    - Change the classloader to locate the agent path in AgentPackagePath, from `SystemClassLoader` to AgentPackagePath's loader.

--- apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/boot/AgentPackagePath.java
+++ apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/boot/AgentPackagePath.java
@@ -49,7 +49,7 @@ public class AgentPackagePath {
     private static File findPath() throws AgentPackageNotFoundException {
         String classResourcePath = AgentPackagePath.class.getName().replaceAll("\\.", "/") + ".class";
 
-        URL resource = ClassLoader.getSystemClassLoader().getResource(classResourcePath);
+        URL resource = AgentPackagePath.class.getClassLoader().getResource(classResourcePath);
         if (resource != null) {
             String urlString = resource.toString();
 

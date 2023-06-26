79f8d01779c1a3d58c2448a266249de94359b078
dd1k <hugsaress@gmail.com>
2023-06-26 21:32:39 +0800

    Add plugin to support Aerospike Java client

---
+++ apm-sniffer/apm-sdk-plugin/aerospike-plugin/src/main/java/org/apache/skywalking/apm/plugin/aerospike/AerospikeMethodMatch.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.skywalking.apm.plugin.aerospike;
+
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+public enum AerospikeMethodMatch {
+    INSTANCE;
+
+    public ElementMatcher<MethodDescription> getAerospikeMethodMatcher() {
+        return named("append")
+                .or(named("put"))
+                .or(named("prepend"))
+                .or(named("add"))
+                .or(named("delete"))
+                .or(named("truncate"))
+                .or(named("touch"))
+                .or(named("exists"))
+                .or(named("get"))
+                .or(named("getHeader"))
+                .or(named("operate"))
+                .or(named("scanAll"))
+                .or(named("scanNode"))
+                .or(named("scanPartitions"))
+                .or(named("getLargeList"))
+                .or(named("getLargeMap"))
+                .or(named("getLargeSet"))
+                .or(named("getLargeStack"))
+                .or(named("register"))
+                .or(named("registerUdfString"))
+                .or(named("removeUdf"))
+                .or(named("execute"))
+                .or(named("query"))
+                .or(named("queryNode"))
+                .or(named("queryPartitions"))
+                .or(named("queryAggregate"))
+                .or(named("queryAggregateNode"))
+                .or(named("createIndex"))
+                .or(named("dropIndex"))
+                .or(named("info"))
+                .or(named("setXDRFilter"))
+                .or(named("createUser"))
+                .or(named("dropUser"))
+                .or(named("changePassword"))
+                .or(named("grantRoles"))
+                .or(named("revokeRoles"))
+                .or(named("createRole"))
+                .or(named("dropRole"))
+                .or(named("grantPrivileges"))
+                .or(named("revokePrivileges"))
+                .or(named("setWhitelist"))
+                .or(named("setQuotas"))
+                .or(named("queryUser"))
+                .or(named("queryUsers"))
+                .or(named("queryRole"))
+                .or(named("queryRoles"))
+                .or(named("queryRoles"));
+    }
+}
---
+++ test/plugin/scenarios/aerospike-scenario/src/main/java/org/apache/skywalking/apm/testcase/aerospike/controller/AerospikeCommandExecutor.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.apm.testcase.aerospike.controller;
+
+import com.aerospike.client.*;
+import com.aerospike.client.cdt.*;
+import com.aerospike.client.policy.*;
+
+public class AerospikeCommandExecutor implements AutoCloseable {
+    private final AerospikeClient client;
+    private final WritePolicy defaultWritePolicy = new WritePolicy();
+    private final WritePolicy defaultReadPolicy = new WritePolicy();
+    private static final String VALUENAME = "DATA";
+    private static final String namespace = "test";
+    private static final String setName = "skywalking";
+
+
+    public AerospikeCommandExecutor(String host, Integer port) {
+        client = new AerospikeClient(host, port);
+    }
+
+    public void set(String key, String value) {
+        final Key k = new Key(namespace, setName, key);
+        final Bin bin = new Bin(VALUENAME, value);
+
+        client.put(defaultWritePolicy, k, bin);;
+    }
+
+    public void exists(String key) {
+        final Key k = new Key(namespace, setName, key);
+
+        client.exists(null, k);
+    }
+
+    public void get(String key) {
+        final Key k = new Key(namespace, setName, key);
+
+        client.get(defaultReadPolicy, k);
+    }
+
+    public void append(String key, String value) {
+        final Key k = new Key(namespace, setName, key);
+        final Bin bin = new Bin(VALUENAME, value);
+
+        client.append(defaultWritePolicy, k, bin);;
+    }
+
+    public void operate(String key, long by) {
+        final Key k = new Key(namespace, setName, key);
+
+        client.operate(defaultWritePolicy, k, Operation.add(new Bin(VALUENAME, by)), Operation.get(VALUENAME));
+    }
+
+    public void delete(String key) {
+        final Key k = new Key(namespace, setName, key);
+
+        client.delete(null, k);
+    }
+
+    public void close() throws Exception {
+        client.close();
+    }
+}

5564d30439ee406e9c5a8c1b8a107b3aafb81eaf
peachisai <497533062@qq.com>
2023-10-23 20:19:26 +0800

    fix netty operation

--- apm-sniffer/optional-plugins/netty-http-4.1.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/netty/http/common/NettyConstants.java
+++ apm-sniffer/optional-plugins/netty-http-4.1.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/netty/http/common/NettyConstants.java
@@ -20,6 +20,8 @@ package org.apache.skywalking.apm.plugin.netty.http.common;
 
 public class NettyConstants {
 
+    public static final String NETTY_HTTP = "Netty-http";
+
     public static final String HTTP_PROTOCOL_PREFIX = "http://";
 
     public static final String HTTPS_PROTOCOL_PREFIX = "https://";

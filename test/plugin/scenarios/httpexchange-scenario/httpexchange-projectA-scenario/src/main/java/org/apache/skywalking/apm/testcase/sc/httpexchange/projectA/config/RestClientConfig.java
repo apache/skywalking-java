c4bd8c5c5284b7d1ca806acc6a4014a6150c5fbd
Claire <clairelove.chen@gmail.com>
2024-01-11 16:41:42 +0800

    Fix checkstyle errors and add webclient reference in Supported-list.md

--- test/plugin/scenarios/httpexchange-scenario/httpexchange-projectA-scenario/src/main/java/org/apache/skywalking/apm/testcase/sc/httpexchange/projectA/config/RestClientConfig.java
+++ test/plugin/scenarios/httpexchange-scenario/httpexchange-projectA-scenario/src/main/java/org/apache/skywalking/apm/testcase/sc/httpexchange/projectA/config/RestClientConfig.java
@@ -23,7 +23,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
-import org.springframework.web.service.invoker.HttpClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration

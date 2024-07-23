1e2b82a7afde53ffed764a5f8cf9c4fad2d8e243
darkness <422019747@qq.com>
2024-07-23 09:41:36 +0800

    add support for kafka3.7.x, because kafka3.7 changed the pull message method's name from pollForFetches to poll

--- apm-sniffer/apm-sdk-plugin/kafka-plugin/src/main/java/org/apache/skywalking/apm/plugin/kafka/Kafka37ConsumerInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/kafka-plugin/src/main/java/org/apache/skywalking/apm/plugin/kafka/Kafka37ConsumerInterceptor.java
@@ -20,6 +20,7 @@ package org.apache.skywalking.apm.plugin.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.header.Header;
 import org.apache.skywalking.apm.agent.core.context.CarrierItem;
 import org.apache.skywalking.apm.agent.core.context.ContextCarrier;
@@ -32,7 +33,7 @@ import org.apache.skywalking.apm.network.trace.component.ComponentsDefine;
 
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * kafak3.7 removed the method named pollForFetches with return type Fetch.
@@ -43,36 +44,22 @@ import java.util.Iterator;
 public class Kafka37ConsumerInterceptor extends KafkaConsumerInterceptor {
 
     @Override
-    public Object afterMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, Object ret) throws Throwable {
-        if (ret instanceof ConsumerRecords) {
-            ConsumerEnhanceRequiredInfo requiredInfo = (ConsumerEnhanceRequiredInfo) objInst.getSkyWalkingDynamicField();
-            ConsumerRecords<?, ?> consumerRecords = (ConsumerRecords<?, ?>) ret;
-            if (consumerRecords.count() == 0) {
-                return ret;
+    protected Map<TopicPartition, List<ConsumerRecord<?, ?>>> fetchRecords(Object retObj) {
+        Map<TopicPartition, List<ConsumerRecord<?, ?>>> rsp = new HashMap<>();
+        if (retObj instanceof ConsumerRecords) {
+            ConsumerRecords<?, ?> consumerRecords = (ConsumerRecords<?, ?>) retObj;
+            if (consumerRecords.isEmpty()) {
+                return rsp;
             }
             for (ConsumerRecord<?, ?> consumerRecord : consumerRecords) {
-                if (consumerRecord == null) {
-                    continue;
-                }
-                ContextCarrier contextCarrier = new ContextCarrier();
-                CarrierItem next = contextCarrier.items();
-                while (next.hasNext()) {
-                    next = next.next();
-                    Iterator<Header> iterator = consumerRecord.headers().headers(next.getHeadKey()).iterator();
-                    if (iterator.hasNext()) {
-                        next.setHeadValue(new String(iterator.next().value(), StandardCharsets.UTF_8));
-                    }
-                }
-                String operationName = OPERATE_NAME_PREFIX + requiredInfo.getTopics() + CONSUMER_OPERATE_NAME + requiredInfo.getGroupId();
-                AbstractSpan activeSpan = ContextManager.createEntrySpan(operationName, contextCarrier).start(requiredInfo.getStartTime());
-                activeSpan.setComponent(ComponentsDefine.KAFKA_CONSUMER);
-                SpanLayer.asMQ(activeSpan);
-                Tags.MQ_BROKER.set(activeSpan, requiredInfo.getBrokerServers());
-                Tags.MQ_TOPIC.set(activeSpan, requiredInfo.getTopics());
-                activeSpan.setPeer(requiredInfo.getBrokerServers());
-                ContextManager.stopSpan();
+                int partition = consumerRecord.partition();
+                String topic = consumerRecord.topic();
+                TopicPartition topicPartition = new TopicPartition(topic, partition);
+                List<ConsumerRecord<?, ?>> recordsForTp = rsp.computeIfAbsent(topicPartition, k -> new ArrayList<>());
+                recordsForTp.add(consumerRecord);
             }
         }
-        return ret;
+        return rsp;
     }
+
 }

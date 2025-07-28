6253528600c4694c344fd7389e0e17be7ab4cc63
wang <wang@wangdeMacBook-Pro.local>
2025-07-28 19:58:36 +0800

    update test scenario

---
+++ test/plugin/scenarios/spring-4.1.x-scenario/src/main/java/test/apache/skywalking/apm/testcase/spring3/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package test.apache.skywalking.apm.testcase.spring3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+}

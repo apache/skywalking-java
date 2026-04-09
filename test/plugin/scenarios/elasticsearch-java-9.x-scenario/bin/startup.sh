d1c9bc7bade2f6fa2e17fe7627495201a3057859
Wu Sheng <wu.sheng@foxmail.com>
2026-04-09 23:08:42 +0800

    Split elasticsearch-java 9.x test to JDK 17 scenario
    elasticsearch-java 9.0.0 is compiled for JDK 17 (class version 61),
    so it cannot be tested in the JDK 8 test container. Split into a
    separate elasticsearch-java-9.x-scenario using Spring Boot 3.x and
    JDK 17 compiler target, added to plugins-jdk17-test.1.yaml.

--- test/plugin/scenarios/elasticsearch-java-9.x-scenario/bin/startup.sh
+++ test/plugin/scenarios/elasticsearch-java-9.x-scenario/bin/startup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,11 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# co.elastic.clients:elasticsearch-java
-# ES server fixed at 8.17.0 (backward compatible with 7.x/8.x/9.x clients)
-# 7.x: RestClientTransport.performRequest directly
-# 8.x/9.x: ElasticsearchTransportBase.performRequest
-7.17.22
-8.5.3
-8.12.2
-8.17.0
+home="$(cd "$(dirname $0)"; pwd)"
+
+java -jar ${agent_opts} -Dskywalking.plugin.elasticsearch.trace_dsl=true ${home}/../libs/elasticsearch-java-9.x-scenario.jar &
